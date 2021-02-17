a471182414c149d1f55f61ae0dc50cb563475223
VicenteVivan <vicente.vivancoc@gmail.com>
2021-02-16 22:39:59 -0300

    Changed File Path

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
